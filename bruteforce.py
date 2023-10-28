9345ada6b04a66f4ba11130aec96458c339eb124
Kerellos Ghaly <93882046+TheKingLol@users.noreply.github.com>
2023-10-28 15:39:23 +0100

    Hashrate update
    Hashrate monitor has only been added to optimised brute force

--- bruteforce.py
+++ bruteforce.py
@@ -1,8 +1,9 @@
 from bit import Key
 from multiprocessing import cpu_count, Process
 from requests import get
-from time import sleep
+from time import time, sleep
 import os
+import threading
 
 # Function to read addresses from files in a directory
 def read_addresses_from_directory(directory):
@@ -25,9 +26,16 @@ wallets = read_addresses_from_directory(input_directory)
 
 max_p = 115792089237316195423570985008687907852837564279074904382605163141518161494336
 
-# random bruteforce
-# Will randomly generate addresses
-def RBF(r, sep_p):
+# Lock for thread safety when updating hashrate and addresses generated
+hashrate_lock = threading.Lock()
+addresses_generated_lock = threading.Lock()
+
+# Initialize variables for hashrate monitoring
+hashrate = 0
+addresses_generated = 0
+
+# Randomized Brute Force function
+def random_brute_force(r, sep_p):
     print(f'Instance: {r + 1} - Generating random addresses...')
     while True:
         pk = Key()
@@ -36,20 +44,27 @@ def RBF(r, sep_p):
             with open(output_file, 'a') as result:
                 result.write(f'{pk.to_wif()}\n')
 
-# random bruteforce output
-def debug_RBF(r, sep_p):
-    print(f'Instance: {r + 1} - Generating random addresses...')
-    while True:
-        pk = Key()
-        print(f'Instance: {r + 1} - Generated: {pk.address}')
-        if pk.address in wallets:
-            print(f'Instance: {r + 1} - Found: {pk.address}')
-            with open(output_file, 'a') as result:
-                result.write(f'{pk.to_wif()}\n')
+        # Update addresses generated count
+        with addresses_generated_lock:
+            global addresses_generated
+            addresses_generated += 1
 
-# traditional bruteforce (slowest)
-# Will try every INT from 0 to max possible
-def TBF(r, sep_p):
+# Function to calculate hashrate
+def calculate_hashrate():
+    global hashrate, addresses_generated
+    while True:
+        sleep(10)  # Calculate hashrate every 10 seconds
+        with hashrate_lock:
+            hashrate = addresses_generated / 10  # Calculate hashrate as addresses per second
+            addresses_generated = 0
+
+# Start the hashrate calculation thread
+hashrate_thread = threading.Thread(target=calculate_hashrate)
+hashrate_thread.daemon = True
+hashrate_thread.start()
+
+# Traditional Brute Force function
+def traditional_brute_force(r, sep_p):
     sint = sep_p * r if sep_p * r != 0 else 1
     mint = sep_p * (r + 1)
     print(f'Instance: {r + 1} - Generating addresses...')
@@ -60,23 +75,19 @@ def TBF(r, sep_p):
             with open(output_file, 'a') as result:
                 result.write(f'{pk.to_wif()}\n')
         sint += 1
-    print(f'Instance: {r + 1}  - Done')
 
-# online bruteforce (randomized)
-def OBF():
+# Online Brute Force function
+def online_brute_force():
     print('Instance: 1 - Generating random addresses...')
     while True:
         pk = Key()
-
         print(f'Instance: 1 - Generated: {pk.address} wif: {pk.to_wif()}')
         print('Instance: 1 - Checking balance...')
-
         try:
             balance = int(get(f'https://blockchain.info/q/addressbalance/{pk.address}/').text)
         except ValueError:
             print(f'Instance: 1 - Error reading balance from: {pk.address}')
             continue
-
         print(f'Instance: 1 - {pk.address} has balance: {balance}')
         if balance > 0:
             with open(output_file, 'a') as result:
@@ -85,8 +96,19 @@ def OBF():
         print('Sleeping for 10 seconds...')
         sleep(10)
 
-# traditional bruteforce output
-def debug_TBF(r, sep_p):
+# Debug version of Randomized Brute Force
+def debug_random_brute_force(r, sep_p):
+    print(f'Instance: {r + 1} - Generating random addresses...')
+    while True:
+        pk = Key()
+        print(f'Instance: {r + 1} - Generated: {pk.address}')
+        if pk.address in wallets:
+            print(f'Instance: {r + 1} - Found: {pk.address}')
+            with open(output_file, 'a') as result:
+                result.write(f'{pk.to_wif()}\n')
+
+# Debug version of Traditional Brute Force
+def debug_traditional_brute_force(r, sep_p):
     sint = sep_p * r if sep_p * r != 0 else 1
     mint = sep_p * (r + 1)
     print(f'Instance: {r + 1} - Generating addresses...')
@@ -98,12 +120,32 @@ def debug_TBF(r, sep_p):
             with open(output_file, 'a') as result:
                 result.write(f'{pk.to_wif()}\n')
         sint += 1
-    print(f'Instance: {r + 1}  - Done')
 
-# optimized traditional bruteforce
-# Will try every INT between 10**75 and max possibility.
-# This method is based on the best practice to get the safest address possible.
-def OTBF(r, sep_p):
+# Debug version of Online Brute Force
+def debug_online_brute_force():
+    print('Instance: 1 - Generating random addresses...')
+    while True:
+        pk = Key()
+        print(f'Instance: 1 - Generated: {pk.address} wif: {pk.to_wif()}')
+        print('Instance: 1 - Checking balance...')
+        try:
+            balance = int(get(f'https://blockchain.info/q/addressbalance/{pk.address}/').text)
+        except ValueError:
+            print(f'Instance: 1 - Error reading balance from: {pk.address}')
+            continue
+        print(f'Instance: 1 - {pk.address} has balance: {balance}')
+        if balance > 0:
+            with open(output_file, 'a') as result:
+                result.write(f'{pk.to_wif()}\n')
+            print(f'Instance: 1 - Added address to {output_file}')
+        print('Sleeping for 10 seconds...')
+        sleep(10)
+
+# Optimized Traditional Brute Force function with hashrate monitoring
+def optimized_traditional_brute_force_with_hashrate(r, sep_p, time_interval=10):
+    start_time = time()
+    addresses_generated = 0
+
     sint = (sep_p * r) + 10 ** 75 if r == 0 else (sep_p * r)
     mint = (sep_p * (r + 1))
     print(f'Instance: {r + 1} - Generating addresses...')
@@ -114,10 +156,25 @@ def OTBF(r, sep_p):
             with open(output_file, 'a') as result:
                 result.write(f'{pk.to_wif()}\n')
         sint += 1
-    print(f'Instance: {r + 1}  - Done')
 
-# optimized traditional bruteforce output
-def debug_OTBF(r, sep_p):
+        addresses_generated += 1
+
+        # Calculate hashrate
+        elapsed_time = time() - start_time
+        if elapsed_time >= time_interval:
+            with hashrate_lock:
+                hashrate = addresses_generated / elapsed_time
+                print(f'Instance: {r + 1} - Hashrate: {hashrate:.2f} addresses per second')
+
+                # Reset counters
+                start_time = time()
+                addresses_generated = 0
+
+# Debug version of Optimized Traditional Brute Force
+def debug_optimized_traditional_brute_force(r, sep_p, time_interval=10):
+    start_time = time()
+    addresses_generated = 0
+
     sint = (sep_p * r) + 10 ** 75 if r == 0 else (sep_p * r)
     mint = (sep_p * (r + 1))
     print(f'Instance: {r + 1} - Generating addresses...')
@@ -129,11 +186,33 @@ def debug_OTBF(r, sep_p):
             with open(output_file, 'a') as result:
                 result.write(f'{pk.to_wif()}\n')
         sint += 1
-    print(f'Instance: {r + 1}  - Done')
+
+        addresses_generated += 1
+
+        # Calculate hashrate
+        elapsed_time = time() - start_time
+        if elapsed_time >= time_interval:
+            with hashrate_lock:
+                hashrate = addresses_generated / elapsed_time
+                print(f'Instance: {r + 1} - Hashrate: {hashrate:.2f} addresses per second')
+
+                # Reset counters
+                start_time = time()
+                addresses_generated = 0
 
 def main():
     # Set bruteforce mode
-    mode = (None, RBF, TBF, OTBF, OBF, debug_RBF, debug_TBF, debug_OTBF)
+    mode = (
+        None,
+        random_brute_force,
+        traditional_brute_force,
+        optimized_traditional_brute_force_with_hashrate,
+        online_brute_force,
+        debug_random_brute_force,
+        debug_traditional_brute_force,
+        debug_optimized_traditional_brute_force,
+        debug_online_brute_force
+    )
 
     # Print menu
     menu_string = 'Select bruteforce mode:\n'
@@ -164,24 +243,24 @@ def main():
         option = 0
         cpu_cores = 0
 
-    if mode[option] and mode[option].__name__ != 'OBF':
+    if mode[option] and mode[option].__name__ != 'online_brute_force':
+        time_interval = 10  # Set your desired time interval for hashrate monitoring (in seconds)
         print(f'Starting bruteforce instances in mode: {mode[option].__name__} with {cpu_cores} core(s)\n')
 
         instances = []
         for i in range(cpu_cores):
-            instance = Process(target=mode[option], args=(i, round(max_p / cpu_cores)))
+            instance = Process(target=mode[option], args=(i, round(max_p / cpu_cores), time_interval))
             instances.append(instance)
             instance.start()
 
         for instance in instances:
             instance.join()
 
-    elif mode[option].__name__ == 'OBF':
+    elif mode[option].__name__ == 'online_brute_force':
         print(f'Starting bruteforce in mode: {mode[option].__name__} (6 per minute to respect API rate limit)\n')
-        OBF()
+        online_brute_force()
 
     print('Stopping...')
 
 if __name__ == '__main__':
     main()
-
